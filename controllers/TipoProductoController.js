f83bef9ff5a8590a0c93152dc188052ced8ebb06
Gaspar356 <gasparandres2002@gmail.com>
2023-11-09 20:04:11 -0300

    tipoproductoback

--- controllers/TipoProductoController.js
+++ controllers/TipoProductoController.js
@@ -4,17 +4,52 @@ const {Producto} = require("../models");
 const TipoProducto = require('../models').TipoProducto;
 
 module.exports = {
-    create(req, res) {
-        /*
-        return Producto
-            .create({
-                // Values
-            })
-            .then(usuario => res.status(200).send(Producto))
-            .catch(error => res.status(400).send(error))
 
-         */
-    },
+    async Create (req, res) {
+  try {
+    const { nombre } = req.body;
+
+    const tipoProducto = await TipoProducto.create({ nombre });
+    res.status(200).send({ message: 'Tipo de producto creado correctamente', data: tipoProducto });
+  } catch (error) {
+    res.status(400).send({ error: 'Error al crear tipo de producto', details: error.message });
+  }
+},
+    async deleteTipoProducto (req, res)  {
+  try {
+    const tipoProductoId = req.params.id;
+
+    const resultado = await TipoProducto.destroy({ where: { id: tipoProductoId } });
+
+    if (resultado === 1) {
+      res.status(200).send({ message: 'Tipo de producto eliminado correctamente' });
+    } else {
+      res.status(404).send({ error: 'No se encontró el tipo de producto con el ID especificado' });
+    }
+  } catch (error) {
+    res.status(400).send({ error: 'Error al eliminar tipo de producto', details: error.message });
+  }
+},
+
+// Modificación (Update)
+ async update (req, res) {
+  try {
+    const tipoProductoId = req.params.id;
+    const { nombre } = req.body;
+
+    const resultado = await TipoProducto.update({ nombre }, { where: { id: tipoProductoId } });
+
+    if (resultado[0] === 1) {
+      res.status(200).send({ message: 'Tipo de producto actualizado correctamente' });
+    } else {
+      res.status(404).send({ error: 'No se encontró el tipo de producto con el ID especificado' });
+    }
+  } catch (error) {
+    res.status(400).send({ error: 'Error al actualizar tipo de producto', details: error.message });
+  }
+},
+
+
     list(req, res) {
         return TipoProducto.findAll({
             attributes: {exclude: ['createdAt', 'updatedAt']}
